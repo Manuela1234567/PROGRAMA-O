14b357a335195e9d06da391e1dab79386b271384
Manuela1234567 <159146343+Manuela1234567@users.noreply.github.com>
2024-05-21 09:17:37 -0300

    Add files via upload

--- sequencia.py
+++ sequencia.py
@@ -1,15 +1,15 @@
 def bombom(dinheiro,preco):
-return float(dinheiro)/preco, dinheiro%preco
-
-def bombom(dinheiro,preco):
-return dinheiro//preco, dinheiro%preco
-
-
-def bombom(dinheiro,preco):
-return dinheiro//preco, dinheiro%preco
+    return float(dinheiro)/preco, dinheiro%preco
 
 def maisbombom(dinheiro,preco):
-return preco – bombom(dinheiro,preco)[1]
+    return preco_bombom(dinheiro,preco)[1]
 
+def numeros_pares(numero):
+    lista_pares = []
+    for i in range(2, numero + 1, 2):
+        lista_pares.append(i)
+    return lista_pares
 
-
+# Exemplo de uso:
+numero = 10
+print(numeros_pares(numero))
